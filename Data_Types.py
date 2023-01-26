e67fc0cac0652dcadfb9c3ed4ecf424cde7ed329
KasymNazar <123566889+KasymNazar@users.noreply.github.com>
2023-01-26 15:34:19 +0600

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,33 @@
+#Exercise 1
+x = 5
+print (type(x))
+# int
+
+#Exercise 2
+x = "Hello World"
+print (type(x))
+# str
+
+#Exercise 3
+x = 20.5
+print(type(x))
+# float
+
+#Exercie 4
+x = ["apple","banana","cherry"]
+print (type(x))
+# list
+
+#Exercise 5
+x = ("aplle", "banana", "cherry")
+print (type(x))
+# tuple
+
+#Exercise 6
+x = {"name": "John", "age": 36}
+print (type(x))
+# dict
+
+#Exercise 7
+x = Trueprint(type(x))
+# bool
---
+++ Syntax.py
@@ -0,0 +1,5 @@
+#Exercise 1
+print ("Hello ,World!")
+#Exercise 2
+if 5>2:
+    print("Five is greater than two!")
